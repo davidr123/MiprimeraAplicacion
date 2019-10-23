0273c09a3661e31ce807d12a93c0d6e03d2a151a
davidr123 <davidradaalejandro@gmail.com>
2019-10-23 17:28:29 -0500

    Initial commit

---
+++ app/src/main/java/com/example/prueba/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.prueba;
+
+import android.app.Activity;
+import android.content.Context;
+import android.graphics.Color;
+import android.os.Bundle;
+
+import com.github.johnpersano.supertoasts.library.Style;
+import com.github.johnpersano.supertoasts.library.SuperActivityToast;
+import com.github.johnpersano.supertoasts.library.utils.PaletteUtils;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+        SuperActivityToast.create(this, new Style(), Style.TYPE_BUTTON)
+
+                .setButtonText("UNDO")
+                .setOnButtonClickListener("good_tag_name", null, null)
+                .setProgressBarColor(Color.WHITE)
+                .setText("Email deleted")
+                .setDuration(Style.DURATION_LONG)
+                .setFrame(Style.FRAME_LOLLIPOP)
+                .setColor(PaletteUtils.getSolidColor(PaletteUtils.MATERIAL_PURPLE))
+                .setAnimations(Style.ANIMATIONS_POP).show();
+
+    }
+
+
+
+
+}
---
+++ app/src/main/java/com/example/prueba/ejemplojava.java
@@ -0,0 +1,12 @@
+package com.example.prueba;
+
+import android.content.Context;
+import android.widget.Toast;
+
+public class ejemplojava {
+
+    public static void myejemplojava(Context context){
+        Toast.makeText(context, "oye", Toast.LENGTH_SHORT).show();
+
+    }
+}
